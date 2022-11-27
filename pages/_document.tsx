3d97cb14d625ad12c7548c8dd33ce4577cbd60fe
Pawel Fajfer <fajfer.pawel@gmail.com>
2022-11-27 11:03:11 +0100

    Add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,21 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 class MyDocument extends Document {
 
   render() {
     return (
       <Html lang="pl">
-        <Head/>
+        <Head>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></script>
+            <script dangerouslySetInnerHTML={{__html:`window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', ${GA_TRACKING_ID});
+              `}}>
+            </script>
+        </Head>
         <body>
         <Main />
         <NextScript />
