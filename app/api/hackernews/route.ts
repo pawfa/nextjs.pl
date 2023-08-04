00db1b12918996d740721f0fa9a7d5468b91e602
Pawel Fajfer <fajfer.pawel@gmail.com>
2023-08-04 11:54:22 +0200

    Added hacker news update with serverless function

---
+++ app/api/hackernews/route.ts
@@ -0,0 +1,53 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { parse } from 'node-html-parser';
+import {db} from "@vercel/postgres";
+
+export async function GET(request: NextRequest) {
+
+    const respHTML = await fetch("https://news.ycombinator.com/from?site=nextjs.org",{ cache: 'no-store' })
+    const html = await respHTML.text()
+
+    const root = parse(html)
+    const titleSpan = root.querySelectorAll('span[class="titleline"]')
+
+    const titleData = titleSpan.map((span) => {
+        const link = span.querySelector('> a');
+        if (link) {
+            return [link.getAttribute('href'), link.textContent]
+        }
+        return []
+    })
+
+    const subline = root.querySelectorAll('span[class="subline"]')
+    const sublineData =subline.map((span) => {
+        const points = span.querySelector('> span');
+        const links = span.querySelectorAll('a');
+        const arr = [points?.textContent, links[0].textContent, links[1].textContent, links[1].getAttribute('href')]
+
+        links.length === 4 && arr.push(links[3].textContent);
+        return arr
+    })
+    // console.log(points, author, date, id, comments);
+
+    const feeds: any[] = [];
+
+    for (let i = 0; i < titleData.length; i++) {
+        feeds.push({
+            url: titleData[i][0],
+            title: titleData[i][1],
+            points:sublineData[i][0],author: sublineData[i][1],
+            date:sublineData[i][2],
+            itemIdUrl:sublineData[i][3],
+            comments: sublineData[i][4]
+        })
+    }
+
+    const client = await db.connect();
+    for (const feed of feeds) {
+        await client.sql`INSERT INTO hackernews_next_stories (url,title,author,points,date,itemIdUrl,comments) VALUES (${feed.url}, ${feed.title}, ${feed.author}, ${feed.points}, ${feed.date}, ${feed.itemIdUrl}, ${feed.comments});`;
+    }
+    return NextResponse.json({
+        body: feeds,
+    });
+}
--- app/components/HackerNews/HackerNewsFeed.tsx
+++ app/components/HackerNews/HackerNewsFeed.tsx
@@ -1,8 +1,7 @@
 import { sql } from "@vercel/postgres";
-import {format} from 'date-fns';
 
 async function getData() {
-    const { rows } = await sql`SELECT * from hackernews_newstories`;
+    const { rows } = await sql`SELECT * from hackernews_next_stories`;
     return rows
 }
 export const HackerNewsFeed = async () => {
@@ -14,7 +13,7 @@ export const HackerNewsFeed = async () => {
         {data.map((feed)=> {
             return <article key={feed.id}>
                 <h2 style={{fontSize: '1.1rem'}}><a style={{textDecoration: 'underline', color: '#0070f3'}} target={"_blank"} href={feed.url}>{feed.title}</a></h2>
-                <p style={{fontSize: '0.8rem'}}>By <span>{feed.by}</span> | {format(new Date(Number(feed.time)*1000), 'H:mm EEEE LLLL L, y')} | <a href={`https://news.ycombinator.com/item?id=${feed.id}`} target={"_blank"} style={{textDecoration: 'underline'}}>{feed.descendants} Comments</a></p>
+                <p style={{fontSize: '0.8rem'}}><span>{feed.author}</span> | {feed.date} | <a href={`https://news.ycombinator.com/${feed.itemidurl}`} target={"_blank"} style={{textDecoration: 'underline'}}>{feed.comments ? feed.comments : '0 comments'}</a></p>
             </article>
         })}
     </section>
